11c11d5768eaa4fe2550bf42e8d5c6ed96a3f561
Shawn <shawn@foghornconsulting.com>
2018-11-13 11:08:16 -0600

    Created base VM image for Azure.

---
+++ RDGW.tf
@@ -0,0 +1,76 @@
+variable "prefix" {
+  default = "FG"
+}
+
+variable "instance_size" {
+  default = "Standard_DS1_v2"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-RG"
+  location = "West US 2"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = "${azurerm_subnet.internal.id}"
+    private_ip_address_allocation = "dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = "${azurerm_resource_group.main.location}"
+  resource_group_name   = "${azurerm_resource_group.main.name}"
+  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  vm_size               = "${var.prefix}"
+
+  # comment this line to not delete the OS disk automatically when deleting the VM
+   delete_os_disk_on_termination = true
+  # Uncomment this line to not delete the data disks automatically when deleting the VM
+   delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "OS"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_windows_config {
+    enable_automatic_upgrades = true
+    provisionVmAgent = true
+  }
+  tags {
+    environment = "testing"
+  }
+}
